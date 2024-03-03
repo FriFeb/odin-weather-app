68c1cea0b34f8cafd7813b54a1b24525e9fcb8fb
FriFeb <51440204+FriFeb@users.noreply.github.com>
2024-03-03 12:28:14 +0200

    feat: get required data from API call

--- src/index.js
+++ src/index.js
@@ -11,3 +11,53 @@ async function getApiResponseData(endpoint, location) {
     console.log(err);
   }
 }
+
+function getRequiredDayData(dayData) {
+  return {
+    minTemp: {
+      minTemp_C: dayData.mintemp_c,
+      minTemp_F: dayData.mintemp_f,
+    },
+    avgTemp: {
+      avgTemp_C: dayData.avgtemp_c,
+      avgTemp_F: dayData.avgtemp_f,
+    },
+    maxTemp: {
+      maxTemp_C: dayData.maxtemp_c,
+      maxTemp_F: dayData.maxtemp_f,
+    },
+    avgHumidity: dayData.avghumidity,
+    condition: {
+      text: dayData.condition.text,
+      icon: dayData.condition.icon,
+    },
+    uvIndex: dayData.uv,
+  };
+}
+
+function getRequiredHoursData(hoursData) {
+  return hoursData.map((hour) => {
+    return {
+      temp: {
+        temp_C: hour.temp_c,
+        temp_F: hour.temp_f,
+      },
+      humidity: hour.humidity,
+      cloud: hour.cloud,
+    };
+  });
+}
+
+async function getRequiredData(location) {
+  try {
+    const data = await getApiResponseData('forecast.json', location);
+
+    return {
+      date: data.forecast.forecastday[0].date,
+      day: getRequiredDayData(data.forecast.forecastday[0].day),
+      hours: getRequiredHoursData(data.forecast.forecastday[0].hour),
+    };
+  } catch (err) {
+    console.log(err);
+  }
+}
