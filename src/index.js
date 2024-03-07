21991337b38b4edf0a254d8883a35f0ff0968e73
FriFeb <51440204+FriFeb@users.noreply.github.com>
2024-03-07 13:00:16 +0200

    feat(frontend): add page template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { getRequiredWeather } from './backend/weather_handler';
+import './style.css';
 
 const form = document.querySelector('form');
 
@@ -12,18 +13,38 @@ form.addEventListener('submit', async (event) => {
 
 /* 
 TODO: 
+
   Backend:
+
     + chunk code into separate modules
+
     - handle 'no city found' errors
 
   Frontend:
+  
     + brainstorm what I need to show 
       + day - condition, temp, humidity, uv
       + hours - temp, humidity, cloud (as 3 different tabs)
-    - create a template to show the data
+
+    + create a template to show the data
       + create a basic design in figma
-      - write html
-      - add styling
+      + write html
+      + add styling
+
     - append the weather data to the template dynamically
 
+    - add ability to switch between temp modes
+
+    - add info to hours graph 
+      - brainstorm how to show the data
+      - ...
+
+    - add ability to toggle between hour graphs
+
+    - add advanced styling
+      - location input 
+      - temp mode selection
+      - explanations to the day info boxes
+      - hour info mode selected 
+      - hour graph
 */
